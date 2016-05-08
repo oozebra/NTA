1935f99580a7a14bb82d89ac6701f73cb04e63c7
oozebra <deb_baby@live.com>
2016-05-08 15:56:53 -0400

    Service reference added

---
+++ NTA/Tobago.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tobago.aspx.cs" Inherits="NTA.Tobago" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tobago</title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <!--#include file="nav.inc"-->
+        <div class="row">
+        <div class="col-lg-12 col-xs-12 col-md-8">  
+    <div class="table-responsive">
+        <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover"></asp:GridView>
+        </div>
+            </div>
+            </div>
+    
+    </form>
+</body>
+</html>
---
+++ NTA/Tobago.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NTA
+{
+    public partial class Tobago : System.Web.UI.Page
+    {
+        static Configuration rootWebConfig = WebConfigurationManager.OpenWebConfiguration("/NTA");
+        const string CONSTRINGNAME = "NTAConnectionString";
+        ConnectionStringSettings conString = rootWebConfig.ConnectionStrings.ConnectionStrings[CONSTRINGNAME];
+        SqlConnection con;
+        public Member client;
+        ServiceReference1.coursesStreamSoapClient clientt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Session["mem"] != null)
+            {
+                client = (Member)Session["mem"];
+            }
+            else
+            {
+                Response.Redirect("index.aspx");
+            }
+
+            con = new SqlConnection(conString.ConnectionString);
+            try
+            {
+                con.Open();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            clientt = new ServiceReference1.coursesStreamSoapClient();
+            //client.CoursesByLocation("Tobago");
+            GridView1.DataSource = clientt.CoursesByLocation("Tobago");
+            GridView1.DataBind();
+        }
+    }
+}
